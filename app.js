2c8349d07450482b0701b7aa8d5e3cd7e242fb73
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-03-28 11:57:43 +0200

    ft: map operator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const {fromEvent} = rxjs;
 
 function print(val){
+    console.log("val is....", val)
     let element = document.createElement('p');
     element.innerText = val;
     element.style.display = 'flex';
@@ -19,11 +20,20 @@ function print(val){
 // const clicks = rxjs.Observable.fromEvent(document,'click');
 // clicks.subscribe(val => console.log(val))
 
-const promise = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-      resolve('resolved')
-    },1000)
-});
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//       resolve('resolved')
+//     },1000)
+// });
 
-const obsPromise = rxjs.from(promise);
-obsPromise.subscribe(result => print(result));
+// const obsPromise = rxjs.from(promise);
+// obsPromise.subscribe(result => print(result));
+
+
+// operators
+// const numbers = rxjs.of(20,200,3000);
+// const logNumbers = numbers.pipe(rxjs.operators.map(Math.log));
+// logNumbers.subscribe(x => print(x));
+
+// Do operator
+
